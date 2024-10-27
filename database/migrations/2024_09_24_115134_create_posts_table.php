385a9940b57896429e4d9416b5bf4c13f5431284
munseongyun <borygashill608@gmail.com>
2024-10-27 18:16:16 +0900

    🐛fix: : Increase length limit of content column in posts table
    - Updated the length limit of the content column in the posts table to accommodate longer entries.

--- database/migrations/2024_09_24_115134_create_posts_table.php
+++ database/migrations/2024_09_24_115134_create_posts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('content');
+            $table->longText('content');
             $table->string('author');
             $table->string('category');
             $table->timestamps();
